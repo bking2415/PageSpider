66113a19349e90374cfa038e37ae0ae9a065e813
bking2415 <55815159+bking2415@users.noreply.github.com>
2020-02-22 16:14:08 -0500

    update database_utilities.py and page_spider.py added test folder

--- page_spider.py
+++ page_spider.py
@@ -1,9 +1,23 @@
 import os
 import argparse
+from utilities import url_utilities, database_utilities
 
 def main(database: str, url_list_file: str):
+    big_word_list = []
     print("we are going work with " + database)
     print("we are going scan " + url_list_file)
+    urls = url_utilities.load_urls_from_file(url_list_file)
+    for url in urls:
+        print("reading " + url)
+        page_content = url_utilities.load_page(url=url)
+        words = url_utilities.scrape_page(page_contents=page_content)
+        big_word_list.extend(words)
+
+    # Database code
+    os.chdir(os.path.dirname(__file__))
+    path = os.path.join(os.getcwd(), "words.db")
+    database_utilities.create_database(path=path)
+    database_utilities.save_words_to_database(database_path=path, words_list=big_word_list)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
--- utilities/database_utilities.py
+++ utilities/database_utilities.py
@@ -1,7 +1,33 @@
-def create_database(database_path: str):
-    #TODO: generate the database
-    pass
+import sqlite3 as lite
 
-def save_words_to_database(database_path:str, words_list: list):
-    #TODO: save the words to the database
-    pass
+
+def create_database(database_path: str):
+    conn = lite.connect(database_path)
+    with conn:
+        cur = conn.cursor()
+        cur.execute("drop table if exists words")
+        ddl = "create table words(word text not null constraint words_pk primary key, usage_count int default 1 not " \
+              "null); "
+        cur.execute(ddl)
+        ddl = "create unique index words_word_uindex on words (word)"
+        cur.execute(ddl)
+    conn.close()
+
+
+def save_words_to_database(database_path: str, words_list: list):
+    conn = lite.connect(database_path)
+    with conn:
+        cur = conn.cursor()
+        for word in words_list:
+            # check to see if wod is in there
+            sql = "select count(word) from words where word=' " + word + " ' "
+            cur.execute(sql)
+            count = cur.fetchone()[0]
+            if count > 0:
+                sql = "update words set usage_count = usage_count + 1 where word=' " + word + " ' "
+            else:
+                sql = "insert into words(word) values (' " + word + " ')"
+            cur.execute(sql)
+        conn.close()
+        print("Database save complete")
+
